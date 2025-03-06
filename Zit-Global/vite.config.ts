a14ba5ccaf4c19abca776f51e731785c44aaf6d8
alvindogba <alvindr2000@gmail.com>
2025-03-06 14:25:34 +0000

    added build

--- Zit-Global/vite.config.ts
+++ Zit-Global/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure the correct base path
+  build: {
+    outDir: 'dist', // Ensure the build output is correct
+  },
 })
