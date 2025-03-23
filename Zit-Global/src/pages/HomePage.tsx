2683c10f5f01fc6b511395e36306d23655c7da92
alvindogba <alvindr2000@gmail.com>
2025-03-23 03:20:22 +0000

    the ICC page completinon

--- Zit-Global/src/pages/HomePage.tsx
+++ Zit-Global/src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import React, { lazy } from 'react';
 import ScrollToTopButton from '../components/ScrollToTopButton';
-import { Link } from 'lucide-react';
-import MotivationBgImg from "../asset/images/Graduation-Bg-Img.jpg"
 
 // Lazy load components
 const Hero = lazy(() => import('../components/HomePageComponent/Hero/HeroSection'));
@@ -24,29 +22,7 @@ const HomePage: React.FC = () => {
                 <StudyAtZit />
                 <ProgramsWeOffer />
                 <TutorshipSection />
-                <div className="text-white py-16" style={{
-                    backgroundImage: `linear-gradient(rgba(0, 0, 90, 1), rgba(0, 0, 90, 0.98)), url(${MotivationBgImg})`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center'
-                    }}
-                >
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-                    <h2 className="text-2xl font-bold mb-6 text-white">Ready to Start Learning?</h2>
-                    <p className="text-md text-white mb-8 max-w-2xl mx-auto">
-                        Join our community of learners and kickstart your career in technology and design.
-                        All courses are free and designed to help you successed.
-                     </p>
-                    <Link
-                    to="/admission"
-                    className="border border-white font-bold text-xs hover:px-6 hover:py-2 text-white px-3 sm:px-6 py-1 sm:py-2 transition-colors rounded-md inline-flex items-center justify-center"
-                    >
-                 
-                    </Link>
-                    </div>
-                </div>
                 <ReadyToStartSec />
-                <TTM />
                 <TeachSomeone />
                 <TTM />
                 <SuccessStory /> 
